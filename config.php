a6c099ed0bc10d702948c0874cf1d563d1a3521b
vvtommy <yanjingxiaoci@gmail.com>
2013-07-31 21:24:05 +0800

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
     'repository' => 'weistreet_web',
     'branch' => 'dev',
     'execute' => array(
-      'cd /var/www/weistreet-sandbox/web; git reset --hard HEAD 2>&1; git clean -f -d 2>&1; git pull 2>&1; mv index-development.php index.php; mkdir -p protected/runtime'
+      'cd /; git reset --hard HEAD 2>&1; git clean -f -d 2>&1; git pull 2>&1; mv index-development.php index.php; mkdir -p protected/runtime'
     )
   );
 
