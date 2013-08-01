2bbc8f8b760323f38259aa14bcc109f902d1e9c8
jialong_zhang <jialong_zhang@allyes.com>
2013-08-01 21:48:19 +0800

    支持输出浮点数

--- temperature.php
+++ temperature.php
@@ -1,12 +1,12 @@
 <?php
+error_reporting(0);
 exec('sensors',$str);
-var_dump($str);
 preg_match('/:\s+(.+)\s+\(/',$str[7],$matchs);
-$core1=preg_split(' ', trim($matchs[1]));
-$core1=(int)$core1[0];
+$core1=split(' ', trim($matchs[1]));
+$core1=(float)$core1[0];
 preg_match('/:\s+(.+)\s+\(/',$str[8],$matchs);
-$core2=preg_split(' ', trim($matchs[1]));
-$core2=(int)$core2[0];
+$core2=split(' ', trim($matchs[1]));
+$core2=(float)$core2[0];
 
 
 
