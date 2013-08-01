baa84687b2c1b0f0b12f33cf79ae312974238634
Edwin <edwin.zhao@hotmail.com>
2013-08-01 19:19:20 +0800

    rename development and product to web_dev and web_prod add api_dev and api_prod project

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
   $boolDebugLogging = TRUE;
 
-  $arrConfig['development'] = array(
+  $arrConfig['web_dev'] = array(
     'repository' => 'weistreet_web',
     'branch' => 'dev',
     'execute' => array(
@@ -10,10 +10,26 @@
     )
   );
 
-  $arrConfig['product'] = array(
+  $arrConfig['web_prod'] = array(
     'repository' => 'weistreet_web',
     'branch' => 'master',
     'execute' => array(
       'cd /webserver/web_root/www.test.weistreet.com/; git reset --hard HEAD 2>&1; git clean -f -d 2>&1; git pull 2>&1; mv index-product.php index.php; mkdir -p protected/runtime'
+    )
+  );
+
+  $arrConfig['api_dev'] = array(
+    'repository' => 'weistreet_api',
+    'branch' => 'dev',
+    'execute' => array(
+      'cd /webserver/web_root/wxapi.dev.weistreet.com/; git reset --hard HEAD 2>&1; git clean -f -d 2>&1; git pull 2>&1'
+    )
+  );
+
+  $arrConfig['api_prod'] = array(
+    'repository' => 'weistreet_api',
+    'branch' => 'master',
+    'execute' => array(
+      'cd /webserver/web_root/wxapi.weistreet.com/; git reset --hard HEAD 2>&1; git clean -f -d 2>&1; git pull 2>&1'
     )
   );
