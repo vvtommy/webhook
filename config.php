f53e83f91a6000cabc7b98ccc56585c5a2da7bab
Edwin <edwin.zhao@hotmail.com>
2013-08-03 21:47:27 +0800

    修改web配置名称，修改web_dev的目录URL

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
   $boolDebugLogging = TRUE;
 
-  $arrConfig['web_dev'] = array(
+  $arrConfig['web_prod'] = array(
     'repository' => 'weistreet_web',
     'branch' => 'master',
     'execute' => array(
@@ -10,11 +10,11 @@
     )
   );
 
-  $arrConfig['web_prod'] = array(
+  $arrConfig['web_dev'] = array(
     'repository' => 'weistreet_web',
     'branch' => 'dev',
     'execute' => array(
-      'cd /webserver/web_root/www.test.weistreet.com/; git reset --hard HEAD 2>&1; git clean -f -d 2>&1; git pull 2>&1; mv index-product.php index.php; mkdir -p protected/runtime'
+      'cd /webserver/web_root/www.dev.weistreet.com/; git reset --hard HEAD 2>&1; git clean -f -d 2>&1; git pull 2>&1; mv index-product.php index.php; mkdir -p protected/runtime'
     )
   );
 
